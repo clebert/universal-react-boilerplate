821f128b64cd94b0bc7ddd34b8f3069f4a956d4f
Clemens Akens <clebert@me.com>
2015-07-10 11:15:23 +0200

    Fix #9: Add Git pre-commit linting

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,15 @@
 var eslint = require('gulp-eslint');
 var gulp = require('gulp');
 
-gulp.task('default', function () {
+gulp.task('install-hooks', function () {
+    return gulp.src('./hooks/*', {base: './hooks/'}).pipe(gulp.dest('./.git/hooks/'));
+});
+
+gulp.task('lint', function () {
     return gulp.src(['*.js', 'src/**/*.js?(x)'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failOnError());
 });
+
+gulp.task('default', ['lint']);
